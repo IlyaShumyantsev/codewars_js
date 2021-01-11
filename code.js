9de97e7334c6f42b24796d1cd47f0b6a9b7da530
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-11 15:44:39 +0300

    replace A => T, T => A, G => C, C => G

--- code.js
+++ code.js
@@ -1,10 +1,11 @@
-//Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
-//toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-//toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-function toCamelCase(str) {
-  var regExp = /[-_]\w/gi;
-  return str.replace(regExp, function (match) {
-    return match.charAt(1).toUpperCase();
-  });
+// replace A => T, T => A, G => C, C => G
+function DNAStrand(dna) {
+  return [...dna].map((item) => DNAStrand.pairs[item]).join("");
 }
+
+DNAStrand.pairs = {
+  A: "T",
+  T: "A",
+  C: "G",
+  G: "C",
+};
