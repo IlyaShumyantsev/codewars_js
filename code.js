1278a83dcd59c161f7b64004a4ebe91eb5acb275
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-11 15:43:23 +0300

    Complete the solution so that it reverses the string passed into it. 'world'  =>  'dlrow'

--- code.js
+++ code.js
@@ -1,10 +1,2 @@
-//Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
-//toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-//toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-function toCamelCase(str) {
-  var regExp = /[-_]\w/gi;
-  return str.replace(regExp, function (match) {
-    return match.charAt(1).toUpperCase();
-  });
-}
+//Complete the solution so that it reverses the string passed into it. 'world'  =>  'dlrow'
+const reverseString = (str) => str.split("").reverse().join("");
