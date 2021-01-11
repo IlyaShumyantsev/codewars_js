1f450e8fea5cbcfa3f38bb75c7ac5e96bddcc107
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-11 18:11:28 +0300

    Given an array of integers, find the one that appears an odd number of times.

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
-//Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
-//toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-//toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-function toCamelCase(str) {
-  var regExp = /[-_]\w/gi;
-  return str.replace(regExp, function (match) {
-    return match.charAt(1).toUpperCase();
-  });
+// Given an array of integers, find the one that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+function findOdd(A) {
+  const numbers = A.reduce((sum, element) => {
+    sum[element] = (sum[element] || 0) + 1;
+    return sum;
+  }, {});
+  for (const [key, value] of Object.entries(numbers)) {
+    if (value % 2 !== 0) return Number(key);
+  }
+  return "nothing";
 }
