8fbbf6b1184546ccaf7077b7f53d1091615112ea
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-11 15:50:29 +0300

    How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?

--- code.js
+++ code.js
@@ -1,10 +1,21 @@
-//Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
-//toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-//toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-function toCamelCase(str) {
-  var regExp = /[-_]\w/gi;
-  return str.replace(regExp, function (match) {
-    return match.charAt(1).toUpperCase();
-  });
+// A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+// He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+// His mother looks out of a window 1.5 meters from the ground.
+// How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+// Three conditions must be met for a valid experiment:
+//     Float parameter "h" in meters must be greater than 0
+//     Float parameter "bounce" must be greater than 0 and less than 1
+//     Float parameter "window" must be less than h.
+// If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+// Note:
+// The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+// Example:
+// - h = 3, bounce = 0.66, window = 1.5, result is 3
+// - h = 3, bounce = 1, window = 1.5, result is -1 
+// (Condition 2) not fulfilled).
+function bouncingBall(h,  bounce,  window) {
+  if (h > 0 && bounce > 0 && bounce < 1 && window < h) {
+    return h < window ? -1 : 2 + bouncingBall(h * bounce, bounce, window);
+  }
+  return -1;
 }
