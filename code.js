123e321ecf0164294cbed0b275fa712b146e84d7
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-11 15:45:51 +0300

    make negative

--- code.js
+++ code.js
@@ -1,10 +1,4 @@
-//Complete the method/function so that it converts dash/underscore delimited words into camel casing.
-//The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
-//toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
-//toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior"
-function toCamelCase(str) {
-  var regExp = /[-_]\w/gi;
-  return str.replace(regExp, function (match) {
-    return match.charAt(1).toUpperCase();
-  });
+// make negative
+function makeNegative(num) {
+  return num > 0 ? num * -1 : num;
 }
