6b986455df1951642830310181de00498e4c2a89
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-12 11:25:22 +0300

    The solution has been updated using an arrow function

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
 //Your task is to write a function that takes a string and return a new string with all vowels removed.
 //For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
-function disemvowel(str) {
-  return str.replace(/[aeiou]/gi, "");
-}
+const disemvowel = str => str.replace(/[aeiou]/gi, "");
