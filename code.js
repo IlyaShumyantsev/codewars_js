9cfa2e6d26f2702261b8b110e167d969cadcda9e
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-12 11:41:00 +0300

    Merge conflict from task_2 to master is  fixed

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Complete the method/function so that it converts dash/underscore delimited words into camel casing.
 //The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
 //toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
@@ -7,8 +6,8 @@ const toCamelCase = str => {
   let regExp = /[-_](?<letter>\w)/g;
   return str.replace(regExp, (_, letter) => letter.toUpperCase());
 };
-=======
+
 //Your task is to write a function that takes a string and return a new string with all vowels removed.
 //For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 const disemvowel = str => str.replace(/[aeiou]/gi, "");
->>>>>>> task_2
+
