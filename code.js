c4bc654518ff2ef450024cee14dd73c7c80823ed
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-12 12:15:19 +0300

    Added task description

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
-// count X and O
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// Examples input/output:
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
 function XO(str) {
   let counterX = 0, counterO = 0;
   [...str].map((item) => {
