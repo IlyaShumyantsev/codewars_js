e5a21be20f9a7d97bc70f036bfe70aff1c1f00bb
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-12 12:46:16 +0300

    Magic numbers fixed

--- code.js
+++ code.js
@@ -1,9 +1,13 @@
 // Clock shows h hours, m minutes and s seconds after midnight.
 // Your task is to write a function which returns the time since midnight in milliseconds.
 function past(h, m, s) {
-  const condition = h <= 23 && h >= 0 && m <= 59 && m >= 0 && s <= 59 && s >= 0;
+  const MAX_HOUR = 23, MAX_MINUTES = MAX_SECONDS= 59;
+  const MIN_HOUR = MIN_MINUTES = MIN_SECONDS = 0;
+  const SECONDS_IN_MINUTE = 60, SECONDS_IN_HOUR = SECONDS_IN_MINUTE * 60;
+  const MILLI = 1000;
+  const condition = h <= MAX_HOUR && h >= MIN_HOUR && m <= MAX_MINUTES && m >= MIN_MINUTES && s <= MAX_SECONDS && s >= MIN_SECONDS;
   if (condition) {
-    return (h * 3600 + m * 60 + s) * 1000;
+    return (h * SECONDS_IN_HOUR + m * SECONDS_IN_MINUTE + s) * MILLI;
   }
   return -1;
 }
