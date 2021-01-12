267fb00333e2af767953e0bfa88d7230cadf6abd
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-12 12:56:57 +0300

    Changed the name of the function, named the variable more clearly

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 // Beaches are filled with sand, water, fish, and sun.
 // Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
-function sumOfABeach(beach) {
+function getSumOfABeach(beach) {
   let words = ["sand", "water", "fish", "sun"];
   return words.reduce((sum, current) => {
-    let match = beach.toLowerCase().match(new RegExp(current, "g"));
-    return match !== null ? sum += match.length : sum += 0;
+    let matchesList = beach.toLowerCase().match(new RegExp(current, "g"));
+    return matchesList !== null ? sum += matchesList.length : sum += 0;
   }, 0);
 }
