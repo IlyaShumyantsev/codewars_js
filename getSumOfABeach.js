175569db87f9e4633e264d24f51bbd8c081e3e5c
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-14 18:42:38 +0300

    create a new js fire for task_11

--- getSumOfABeach.js
+++ getSumOfABeach.js
@@ -1,5 +1,6 @@
 // Beaches are filled with sand, water, fish, and sun.
 // Given a string, calculate how many times the words "Sand", "Water", "Fish", and "Sun" appear without overlapping (regardless of the case).
+
 function getSumOfABeach(beach) {
   let words = ["sand", "water", "fish", "sun"];
   return words.reduce((sum, current) => {
@@ -7,3 +8,4 @@ function getSumOfABeach(beach) {
     return matchesList !== null ? sum += matchesList.length : sum += 0;
   }, 0);
 }
+
