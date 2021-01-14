74759a25a6021f39855da134179fc13985d2e221
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-14 18:05:05 +0300

    create a new js file for task_1

---
+++ toCamelCase.js
@@ -0,0 +1,10 @@
+// Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+// The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+// toCamelCase("the-stealth-warrior") // returns "theStealthWarrior"
+// toCamelCase("The_Stealth_Warrior") // returns "TheStealthWarrior" 
+
+const toCamelCase = str => {
+  let regExp = /[-_](?<letter>\w)/g;
+  return str.replace(regExp, (_, letter) => letter.toUpperCase());
+};
+
