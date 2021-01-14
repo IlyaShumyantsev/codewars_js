4464e2e45f89f244f357fb8952a68709b78c493f
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-14 18:26:14 +0300

    create a new js file for task_5

---
+++ DNAStrand.js
@@ -0,0 +1,12 @@
+// replace A => T, T => A, G => C, C => G
+function DNAStrand(dna) {
+  return [...dna].map((item) => DNAStrand.pairs[item]).join("");
+}
+
+DNAStrand.pairs = {
+  A: "T",
+  T: "A",
+  C: "G",
+  G: "C",
+};
+
