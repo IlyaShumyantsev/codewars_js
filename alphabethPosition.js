e4d298c3180b814c75fa378505565059b4d8eef3
IlyaShumyantsev <ilya.shumyantsev@gmail.com>
2021-01-14 18:19:28 +0300

    create a new js file for task_3

--- alphabethPosition.js
+++ alphabethPosition.js
@@ -2,6 +2,7 @@
 //If anything in the text isn't a letter, ignore it and don't return it.
 //"a" = 1, "b" = 2, etc.
 //alphabetPosition("The sunset sets at twelve o' clock.")
+
 function alphabetPosition(text) {
   const SHIFT_CHAR_CODE = 64;
   return text
@@ -10,3 +11,4 @@ function alphabetPosition(text) {
     .map((c) => c.charCodeAt() - SHIFT_CHAR_CODE)
     .join(" ");
 }
+
